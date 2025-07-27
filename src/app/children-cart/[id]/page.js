5dea16d8a11047bb418a0f01623e4abae7597614
amirgoody <amirhosynakhlaghian@gmail.com>
2025-07-27 10:29:44 +0330

    add force-dynamic

--- src/app/children-cart/[id]/page.js
+++ src/app/children-cart/[id]/page.js
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from "react";
 import AddBtnCart from "@/components/AddBtnCart/AddBtnCart";
 import { formatNumber } from "@/utils/formatNumber";
