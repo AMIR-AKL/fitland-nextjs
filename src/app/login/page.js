ddd5868e0133b654f709625b4ec237a87c96b381
amirgoody <amirhosynakhlaghian@gmail.com>
2025-08-15 18:39:19 +0330

    add & update login & register page for user

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,145 +1,102 @@
 "use client";
+import DarkVeil from "@/components/DarkWeil/DarkWeil";
 import axios from "axios";
-import Link from "next/link";
 import React, { useState } from "react";
-import { HiOutlineHome } from "react-icons/hi";
-
-export default function Login() {
-	const [name, setName] = useState("");
-	const [phone, setPhone] = useState("");
-	const [email, setEmail] = useState("");
-
-	const [nameError, setNameError] = useState(false);
-	const [phoneError, setPhoneError] = useState(false);
-	const [emailError, setEmailError] = useState(false);
-
-	const nameRegex = /^[آ-ی\s]{3,}$/; // فقط فارسی و حداقل ۳ حرف
-	const phoneRegex = /^09\d{9}$/; // شروع با 09 و 11 رقم
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // ساده ولی معتبر
+import { IoIosLogIn } from "react-icons/io";
+import { IoMdEye } from "react-icons/io";
+export default function page() {
+	const [form, setForm] = useState({ username: "", password: "" });
+	const [inputPasswordType, setInputPasswordType] = useState(true);
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
-		const isNameValid = nameRegex.test(name);
-		const isPhoneValid = phoneRegex.test(phone);
-		const isEmailValid = email === "" || emailRegex.test(email); // اختیاری
-
-		setNameError(!isNameValid);
-		setPhoneError(!isPhoneValid);
-		setEmailError(!isEmailValid);
-
-		if (isNameValid && isPhoneValid && isEmailValid) {
-			const newUser = {
-				id: new Date(),
-				name,
-				phone,
-				email,
-			};
-
-			axios.post("http://localhost:3001/users", newUser).then(() => {
-				setName("");
-				setPhone("");
-				setEmail("");
-			});
+		try {
+			axios
+				.post("https://fitland-gtmr.onrender.com/api/users/login", form)
+				.then((res) => {
+					localStorage.setItem("token", res.data.token);
+					setForm({ username: "", password: "" });
+					window.location.href = "/";
+				});
+		} catch (err) {
+			console.log(err);
 		}
 	};
-
+	const typeInputHandler = (e) => {
+		e.preventDefault();
+		setInputPasswordType(!inputPasswordType);
+	};
 	return (
-		<div className="flex min-h-screen overflow-hidden select-none">
-			{/* Right side */}
-			<div className="w-full lg:w-1/2 relative flex items-center justify-center p-8 bg-white">
-				<div className="absolute top-0 left-0 h-full w-full bg-white/50 z-10"></div>
-				<div className="w-full max-w-md border border-gray-300 p-8 rounded-xl shadow-xl z-50">
-					<h2 className="text-2xl font-iransans-bold">ثبت نام</h2>
-
+		<div className="min-h-screen w-full relative">
+			<DarkVeil hueShift={206} />
+			<div className="w-full absolute top-20 flex items-center justify-center p-8 text-white">
+				<div className="w-full max-w-md border border-gray-300 p-8 rounded-xl shadow-[0_0_8px_5px] shadow-amber-600">
+					<h2 className="text-2xl font-iransans-bold flex items-center gap-2">
+						ورود <IoIosLogIn />
+					</h2>
 					<form onSubmit={handleSubmit} className="mt-8 space-y-4">
 						{/* Name */}
 						<div>
 							<label className="block mb-4 font-iransans-medium tracking-tight">
-								نام و نام خانوادگی*
+								نام کاربری:
 							</label>
-							<input
-								className="w-full h-full border border-gray-300 outline-none p-2 rounded-lg"
-								type="text"
-								placeholder="نام خود را به فارسی وارد کنید"
-								required
-								autoComplete="name"
-								value={name}
-								onChange={(e) => setName(e.target.value)}
-							/>
-							{nameError && (
-								<span className="text-red-500">
-									نام باید حداقل ۳ حرف فارسی باشد
-								</span>
-							)}
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									type="text"
+									placeholder="نام کاربری را وارد کنید"
+									required
+									name="username"
+									autoComplete="name"
+									value={form.username}
+									onChange={handleChange}
+								/>
+								{/* {nameError && (
+									<span className="text-red-500">
+										نام باید حداقل ۳ حرف فارسی باشد
+										</span>
+										)} */}
+							</div>
 						</div>
 
 						{/* Phone */}
 						<div>
 							<label className="block mb-4 font-iransans-medium tracking-tight">
-								شماره تلفن همراه*
+								رمزعبور:
 							</label>
-							<input
-								className="w-full h-full border border-gray-300 outline-none p-2 rounded-lg"
-								type="text"
-								placeholder="09123456789"
-								required
-								autoComplete="tel"
-								value={phone}
-								onChange={(e) => setPhone(e.target.value)}
-							/>
-							{phoneError && (
-								<span className="text-red-500">
-									شماره باید با 09 شروع شده و ۱۱ رقم باشد
-								</span>
-							)}
-						</div>
-
-						{/* Email */}
-						<div>
-							<label className="block mb-4 font-iransans-medium tracking-tight">
-								ایمیل (اختیاری)
-							</label>
-							<input
-								className="w-full h-full border border-gray-300 outline-none p-2 rounded-lg"
-								type="email"
-								placeholder="Example@gmail.com"
-								autoComplete="email"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-							/>
-							{emailError && (
-								<span className="text-red-500">ایمیل وارد شده معتبر نیست</span>
-							)}
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									name="password"
+									onChange={handleChange}
+									type={inputPasswordType ? "password" : "text"}
+									placeholder="رمز عبور خود را وارد کنید"
+									value={form?.password}
+								/>
+								<button
+									onClick={typeInputHandler}
+									className="w-10 py-1 px-2 rounded-xl bg-amber-500 text-white cursor-pointer hover:bg-amber-600 transition-colors"
+								>
+									<IoMdEye className="size-5" />
+								</button>
+							</div>
+							{/* {phoneError && (
+									<span className="text-red-500">
+										شماره باید با 09 شروع شده و ۱۱ رقم باشد
+									</span>
+								)} */}
 						</div>
 
 						<button
 							className="w-full bg-orange-500 hover:bg-orange-600 cursor-pointer transition-colors rounded-lg mt-5 p-2 text-white font-iransans-medium tracking-tight"
 							type="submit"
 						>
-							ثبت نام
+							ورود
 						</button>
 					</form>
-				</div>
-			</div>
-
-			{/* Left side */}
-			<div
-				className="w-1/2 bg-blend-color-burn hidden lg:flex items-center z-10 justify-center bg-zinc-950/70"
-				style={{
-					backgroundImage: "url(./img/signup/signup.jpg)",
-					backgroundSize: "cover",
-					backgroundPosition: "center",
-				}}
-			>
-				<div className="text-white text-center flex flex-col items-center">
-					<h1 className="font-extrabold text-orange-500 text-6xl">FitLand</h1>
-					<div className="flex items-end gap-x-2 text-sm mt-5 hover:text-sky-600 transition-colors">
-						<HiOutlineHome className="size-6" />
-						<Link className="font-iransans-demibold" href="/">
-							برگشت به صفحه اصلی
-						</Link>
-					</div>
 				</div>
 			</div>
 		</div>
--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,5 +1,153 @@
-import React from "react";
+"use client";
+import DarkVeil from "@/components/DarkWeil/DarkWeil";
+import axios from "axios";
+import React, { useState } from "react";
+import { IoMdEye } from "react-icons/io";
 
-export default function SignUp() {
-	return <div>SignUp</div>;
+export default function page() {
+	const [inputPasswordType, setInputPasswordType] = useState(true);
+	const [form, setForm] = useState({
+		firstname: "",
+		lastname: "",
+		username: "",
+		password: "",
+	});
+
+	const typeInputHandler = (e) => {
+		e.preventDefault();
+		setInputPasswordType(!inputPasswordType);
+	};
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		// const isNameValid = nameRegex.test(name);
+		// const isPhoneValid = phoneRegex.test(phone);
+		// const isEmailValid = email === "" || emailRegex.test(email); // اختیاری
+		try {
+			axios.post("https://fitland-gtmr.onrender.com/api/users/register", form).then(() => {
+				window.location.href = "/login";
+			});
+		} catch (err) {
+			console.log(err);
+		}
+
+		// setNameError(!isNameValid);
+		// setPhoneError(!isPhoneValid);
+		// setEmailError(!isEmailValid);
+	};
+	return (
+		<div className="min-h-screen w-full relative">
+			<DarkVeil hueShift={206} />
+			<div className="w-full absolute top-15 flex items-center justify-center p-8 text-white">
+				<div className="w-full max-w-md border border-gray-300 p-8 rounded-xl shadow-[0_0_8px_5px] shadow-amber-600">
+					<h2 className="text-2xl font-iransans-bold">ثبت نام</h2>
+					<form onSubmit={handleSubmit} className="mt-8 space-y-4">
+						{/* firstname */}
+						<div>
+							<label className="block mb-4 font-iransans-medium tracking-tight">
+								نام:
+							</label>
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									type="text"
+									placeholder="نام خود را به فارسی وارد کنید"
+									required
+									name="firstname"
+									onChange={handleChange}
+									autoComplete="name"
+								/>
+								{/* {nameError && (
+								<span className="text-red-500">
+								نام باید حداقل ۳ حرف فارسی باشد
+								</span>
+								)} */}
+							</div>
+						</div>
+						{/* lastname */}
+						<div>
+							<label className="block mb-4 font-iransans-medium tracking-tight">
+								نام خانوادگی:
+							</label>
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									type="text"
+									placeholder="نام خانوادگی خود را به فارسی وارد کنید"
+									required
+									name="lastname"
+									onChange={handleChange}
+									autoComplete="name"
+								/>
+								{/* {nameError && (
+								<span className="text-red-500">
+								نام باید حداقل ۳ حرف فارسی باشد
+								</span>
+								)} */}
+							</div>
+						</div>
+						{/* username */}
+						<div>
+							<label className="block mb-4 font-iransans-medium tracking-tight">
+								نام کاربری:
+							</label>
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									type="text"
+									placeholder="نام کاربری خود را وارد کنید"
+									required
+									name="username"
+									onChange={handleChange}
+									autoComplete="name"
+								/>
+								{/* {nameError && (
+								<span className="text-red-500">
+								نام باید حداقل ۳ حرف فارسی باشد
+								</span>
+								)} */}
+							</div>
+						</div>
+						{/* password */}
+						<div>
+							<label className="block mb-4 font-iransans-medium tracking-tight">
+								رمزعبور:
+							</label>
+							<div className="flex items-center px-2 h-12 border border-gray-300 rounded-lg font-iransans-regular">
+								<input
+									className="outline-none w-full h-full"
+									placeholder="رمز خود وارد کنید"
+									required
+									name="password"
+									onChange={handleChange}
+									type={inputPasswordType ? "password" : "text"}
+								/>
+								<button
+									onClick={typeInputHandler}
+									className="w-10 py-1 px-2 rounded-xl bg-amber-500 text-white cursor-pointer hover:bg-amber-600 transition-colors"
+								>
+									<IoMdEye className="size-5" />
+								</button>
+								{/* {nameError && (
+								<span className="text-red-500">
+								نام باید حداقل ۳ حرف فارسی باشد
+								</span>
+								)} */}
+							</div>
+						</div>
+						<button
+							className="w-full bg-orange-500 hover:bg-orange-600 cursor-pointer transition-colors rounded-lg mt-5 p-2 text-white font-iransans-medium tracking-tight"
+							type="submit"
+						>
+							ثبت نام
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
