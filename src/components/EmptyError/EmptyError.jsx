8578deec82ad5065c64d7b71d0bde36e44d68a6c
amirgoody <amirhosynakhlaghian@gmail.com>
2025-08-15 18:34:31 +0330

    add emptyerror component

---
+++ src/components/EmptyError/EmptyError.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EmptyError() {
+	return (
+		<div className="bg-red-600 h-40 w-full flex justify-center items-center rounded-lg mt-5 text-white font-iransans-bold text-4xl">
+			محصولی موجود نیست !
+		</div>
+	);
+}
