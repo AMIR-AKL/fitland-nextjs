cfd5440d6071325174a93c2b6948a7df906b3269
amirgoody <amirhosynakhlaghian@gmail.com>
2025-09-11 12:06:26 +0330

    layout changed

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -10,12 +10,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="fa" dir="rtl">
-			<body className="">
-				<FitlandShoppingContextProvider>
-					{children}
-				</FitlandShoppingContextProvider>
-			</body>
-		</html>
+		<FitlandShoppingContextProvider>{children}</FitlandShoppingContextProvider>
 	);
 }
